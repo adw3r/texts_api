25419731aae38636fda35077a59b8316770699ae
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-02-03 16:49:49 +0200

    fixed run_api.cmd, texts.json

--- run_api.cmd
+++ run_api.cmd
@@ -1,3 +1,3 @@
-docker kill texts_api
 docker build -t texts_api .
+docker kill texts_api
 docker run -d --rm -v C:\Users\Administrator\Desktop\texts_api:/myapp --env-file .env --name texts_api -p 8183:8183 texts_api
